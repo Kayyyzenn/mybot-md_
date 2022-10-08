0f1548730e560e450f7498acd5f515f3d87e937e
Not dev <98279739+Kayyyzenn@users.noreply.github.com>
2022-10-09 05:27:52 +0700

    Delete semoji.js

--- plugins/semoji.js
+++
@@ -1,9 +0,0 @@
-const PhoneNumber = require('awesome-phonenumber')
-async function handler(m) {
-                let vcard = "BEGIN:VCARD\nVERSION:3.0\nN:1;;Vinz;;\nFN:Vinz\nitem1.TEL;waid=6285711537991:+62 857-1153-7991\nitem1.X-ABLabel:Ponsel\nX-WA-BIZ-NAME:NdaaBotz-MD\nEND:VCARD"
-                conn.sendMessage(m.chat, { contacts: { contacts: [{ vcard }] } }, { quoted: m })
-}
-
-handler.command = /^(vinzowns)$/i
-
-module.exports = handler
